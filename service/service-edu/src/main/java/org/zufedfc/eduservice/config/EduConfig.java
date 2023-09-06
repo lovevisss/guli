f3729dd27cad7b6c2f9f2f70e58c7f9a65b6cc22
zsy <530822987@qq.com>
2023-09-06 14:50:11 +0800

    mapper get all

---
+++ service/service-edu/src/main/java/org/zufedfc/eduservice/config/EduConfig.java
@@ -0,0 +1,10 @@
+package org.zufedfc.eduservice.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("org.zufedfc.eduservice.mapper")
+public class EduConfig {
+
+}
--- service/service-edu/src/main/java/org/zufedfc/eduservice/controller/EduTeacherController.java
+++ service/service-edu/src/main/java/org/zufedfc/eduservice/controller/EduTeacherController.java
@@ -1,9 +1,15 @@
 package org.zufedfc.eduservice.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.zufedfc.eduservice.entity.EduTeacher;
+import org.zufedfc.eduservice.service.EduTeacherService;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +22,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/eduservice/edu-teacher")
 public class EduTeacherController {
+
+    @Autowired
+    private EduTeacherService teacherService;
+//    1.查询讲师表所有数据
+//    rest风格
+//    GET http://localhost:8001/eduservice/edu-teacher/findAll
+    @GetMapping("findAll")
+    public List<EduTeacher> findAll(){
+        return teacherService.list(null);
+    }
+
+
 
 }
 
