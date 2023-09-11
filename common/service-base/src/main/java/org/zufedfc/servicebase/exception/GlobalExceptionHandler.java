54d0c0c8158a00da491ef44b5b2b65c0c3b06340
zsy <530822987@qq.com>
2023-09-11 22:16:33 +0800

    add error and exception and logback-spring.xml

--- common/service-base/src/main/java/org/zufedfc/servicebase/exception/GlobalExceptionHandler.java
+++ common/service-base/src/main/java/org/zufedfc/servicebase/exception/GlobalExceptionHandler.java
@@ -1,17 +1,20 @@
 package org.zufedfc.servicebase.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.zufedfc.commonutils.R;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseBody //为了返回数据
     public R error(Exception e){
         e.printStackTrace();
+        log.error(e.getMessage());
         return R.error().message("执行了全局异常处理..");
     }
 
